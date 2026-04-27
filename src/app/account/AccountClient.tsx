ac6f114b25402e66a2e7cb6f277bab6cf8645cce
Samuel Phillipson <s267450@uos.ac.uk>
2026-04-27 12:11:01 +0100

    fix: account page type errors for vercel build

--- src/app/account/AccountClient.tsx
+++ src/app/account/AccountClient.tsx
@@ -12,6 +12,7 @@ interface AccountClientProps {
     name?: string | null;
     email?: string | null;
     image?: string | null;
+    username?: string | null;
   };
 }
 
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -6,7 +6,7 @@ import AccountClient from "./AccountClient";
 export default async function AccountPage() {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
+  if (!session || !session.user) {
     redirect("/login");
   }
 
