fb5459eafd3de332e927121b3e6d1d72589e1aff
Samuel Phillipson <s267450@uos.ac.uk>
2026-05-30 08:32:18 +0100

    clean up unused tracked test and model files

--- parse_models.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('models.json', 'utf8'));
-const models = data.models.filter(m => m.supportedGenerationMethods && m.supportedGenerationMethods.includes('generateContent')).map(m => m.name);
-fs.writeFileSync('output.txt', models.join('\n'));
